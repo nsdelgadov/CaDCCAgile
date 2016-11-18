c7b4a7ab07fc27492044464799b9c211e6d915b8
Claudio Berroeta <claudioberroeta@gmail.com>
2016-11-18 07:26:52 +0000

    UPDATE telegram.py

--- cadcc/telegram.py
+++ cadcc/telegram.py
@@ -1,9 +1,12 @@
 import telepot
 
-
+#Credentials
 ID_BOT = "266039242:AAHfHojkbgRyhmGmLuSl1l9z5Vk3mAh4akY"
 ID_CHANNEL = "@testcadcc"
 
 def send_message(message):
+    """Telegram bot sends the message to the telegram channel"""
     bot = telepot.Bot(ID_BOT)
     bot.sendMessage(ID_CHANNEL, message)
+    
+#todo: complete with another methods     
