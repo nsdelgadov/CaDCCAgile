c7f5bc2bfe8d3e58a34f44880b1636cb0342fba4
Claudio Berroeta <claudioberroeta@gmail.com>
2016-11-18 07:25:12 +0000

    ADD telegram bot

--- cadcc/signals/handlers.py
+++ cadcc/signals/handlers.py
@@ -2,9 +2,11 @@ from django.dispatch import receiver
 from django.db.models.signals import post_save
 
 from mezzanine.blog.models import BlogPost
+from cadcc.telegram import send_message
 
 @receiver(post_save, sender=BlogPost)
 def my_handler(sender, **kwargs):
     print("This code executes after the save of any BlogPost")
+    #send_message("This code executes after the save of any BlogPost. Hi telegram!")
     print("sender", sender)
     print("kwargs", kwargs)
